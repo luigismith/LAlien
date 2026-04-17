ce45fcdad6a1f0c344ac822b99a6276e5cb10e59
Luigi Massari <luigi.massari@comes.local>
2026-04-17 14:47:18 +0200

    SW v8 cache bump for items persistence fix

--- web/sw.js
+++ web/sw.js
@@ -3,7 +3,7 @@
  * Cache-first strategy for static assets
  */
 
-const CACHE_NAME = 'lalien-companion-v6';
+const CACHE_NAME = 'lalien-companion-v8';
 const STATIC_ASSETS = [
     './',
     './index.html',
